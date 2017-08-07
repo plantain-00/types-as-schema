959d2070b08f5c42d9ffc9ec8032f24969d4d009
york yao <yaoao12306@outlook.com>
2017-08-07 23:08:26 +0800

    update webpack config; improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -3,28 +3,33 @@ module.exports = {
     `rimraf dist/`,
     `tsc -p src`
   ],
-  lint: [
-    `tslint "src/**/*.ts" "online/**/*.ts"`,
-    `standard "**/*.config.js"`,
-    `stylelint "online/**/*.less"`
-  ],
+  lint: {
+    ts: `tslint "src/**/*.ts" "online/**/*.ts"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "online/**/*.less"`
+  },
   test: [
     'tsc -p spec',
     'jasmine'
   ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ],
-  release: [
-    `clean-release`
-  ],
+  fix: {
+    js: `standard --fix "**/*.config.js"`,
+    less: `stylelint --fix "online/**/*.less"`
+  },
+  release: `clean-release`,
   online: [
-    `rimraf online/*.bundle-*.js online/*.bundle-*.css`,
-    `file2variable-cli online/index.template.html demo/cases.ts -o online/variables.ts --html-minify --base online`,
-    `tsc -p online`,
-    `lessc online/index.less > online/index.css`,
-    `cleancss -o online/index.bundle.css online/index.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
-    `webpack --display-modules --config online/webpack.config.js`,
+    {
+      js: [
+        `file2variable-cli online/index.template.html demo/cases.ts -o online/variables.ts --html-minify --base online`,
+        `tsc -p online`,
+        `webpack --display-modules --config online/webpack.config.js`
+      ],
+      css: [
+        `lessc online/index.less > online/index.css`,
+        `cleancss -o online/index.bundle.css online/index.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
+      ],
+      clean: `rimraf online/*.bundle-*.js online/*.bundle-*.css`
+    },
     `rev-static --config online/rev-static.config.js`
   ],
   'demo:all': [
