5cc6dca8cd94e20aed3bcfb535608f5e15e90e20
york yao <yaoao12306@outlook.com>
2017-08-26 16:37:08 +0800

    add screenshot script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -20,6 +20,7 @@ module.exports = {
       matchCalculator: 'node ./dist/index.js demo/match-calculator/types.ts --json demo/match-calculator/ --debug demo/match-calculator/debug.json',
       baogame: 'node ./dist/index.js demo/baogame/common.ts --protobuf demo/baogame/protocol.proto --debug demo/baogame/debug.json'
     },
+    'git checkout online/screenshot.png',
     () => new Promise((resolve, reject) => {
       childProcess.exec('git status -s', (error, stdout, stderr) => {
         if (error) {
@@ -53,7 +54,19 @@ module.exports = {
       ],
       clean: `rimraf online/*.bundle-*.js online/*.bundle-*.css`
     },
-    `rev-static --config online/rev-static.config.js`
+    `rev-static --config online/rev-static.config.js`,
+    async () => {
+      const { createServer } = require('http-server')
+      const puppeteer = require('puppeteer')
+      const server = createServer()
+      server.listen(8000)
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto(`http://localhost:8000/online`)
+      await page.screenshot({ path: `online/screenshot.png`, fullPage: true })
+      server.close()
+      browser.close()
+    }
   ],
   watch: {
     vue: `file2variable-cli online/index.template.html demo/cases.ts -o online/variables.ts --html-minify --base online --watch`,
