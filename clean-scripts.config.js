258351298ff1a12233e9d503b2d3452d16a89c21
york yao <yaoao12306@outlook.com>
2017-09-17 14:43:52 +0800

    simplify scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -6,39 +6,7 @@ const execAsync = util.promisify(childProcess.exec)
 module.exports = {
   build: [
     `rimraf dist/`,
-    `tsc -p src`
-  ],
-  lint: {
-    ts: `tslint "src/**/*.ts" "online/**/*.ts"`,
-    js: `standard "**/*.config.js"`,
-    less: `stylelint "online/**/*.less"`,
-    export: `no-unused-export "src/**/*.ts" "online/**/*.ts"`
-  },
-  test: [
-    'tsc -p spec',
-    'jasmine',
-    {
-      default: 'node ./dist/index.js demo/cases.ts --json demo/ --debug demo/debug.json --protobuf demo/cases.proto',
-      logTool: 'node ./dist/index.js demo/log-tool/types.ts --json demo/log-tool/ --debug demo/log-tool/debug.json --protobuf demo/log-tool/protocol.proto',
-      matchCalculator: 'node ./dist/index.js demo/match-calculator/types.ts --json demo/match-calculator/ --debug demo/match-calculator/debug.json',
-      baogame: 'node ./dist/index.js demo/baogame/common.ts --protobuf demo/baogame/protocol.proto --debug demo/baogame/debug.json'
-    },
-    'git checkout online/screenshot.png',
-    async () => {
-      const { stdout } = await execAsync('git status -s')
-      if (stdout) {
-        console.log(stdout)
-        throw new Error(`generated files doesn't match.`)
-      }
-    }
-  ],
-  fix: {
-    ts: `tslint --fix "src/**/*.ts" "online/**/*.ts"`,
-    js: `standard --fix "**/*.config.js"`,
-    less: `stylelint --fix "online/**/*.less"`
-  },
-  release: `clean-release`,
-  online: [
+    `tsc -p src`,
     {
       js: [
         `file2variable-cli online/index.template.html demo/cases.ts -o online/variables.ts --html-minify --base online`,
@@ -70,6 +38,36 @@ module.exports = {
       browser.close()
     }
   ],
+  lint: {
+    ts: `tslint "src/**/*.ts" "online/**/*.ts"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "online/**/*.less"`,
+    export: `no-unused-export "src/**/*.ts" "online/**/*.ts"`
+  },
+  test: [
+    'tsc -p spec',
+    'jasmine',
+    {
+      default: 'node ./dist/index.js demo/cases.ts --json demo/ --debug demo/debug.json --protobuf demo/cases.proto',
+      logTool: 'node ./dist/index.js demo/log-tool/types.ts --json demo/log-tool/ --debug demo/log-tool/debug.json --protobuf demo/log-tool/protocol.proto',
+      matchCalculator: 'node ./dist/index.js demo/match-calculator/types.ts --json demo/match-calculator/ --debug demo/match-calculator/debug.json',
+      baogame: 'node ./dist/index.js demo/baogame/common.ts --protobuf demo/baogame/protocol.proto --debug demo/baogame/debug.json'
+    },
+    'git checkout online/screenshot.png',
+    async () => {
+      const { stdout } = await execAsync('git status -s')
+      if (stdout) {
+        console.log(stdout)
+        throw new Error(`generated files doesn't match.`)
+      }
+    }
+  ],
+  fix: {
+    ts: `tslint --fix "src/**/*.ts" "online/**/*.ts"`,
+    js: `standard --fix "**/*.config.js"`,
+    less: `stylelint --fix "online/**/*.less"`
+  },
+  release: `clean-release`,
   watch: {
     vue: `file2variable-cli online/index.template.html demo/cases.ts -o online/variables.ts --html-minify --base online --watch`,
     online: `tsc -p online --watch`,
