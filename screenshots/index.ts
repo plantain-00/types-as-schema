7f4c8c68a49db026bb1222063c0e52ecdb87beb1
york yao <yaoao12306@outlook.com>
2017-11-15 10:09:13 +0800

    update dependencies

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -6,15 +6,15 @@ import * as puppeteer from "puppeteer";
     await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36" });
     await page.goto(`http://localhost:8000/online`);
     await page.waitFor(2000);
-    await page.screenshot({ path: `screenshots/initial.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/initial.png` });
 
     await page.click("button");
     await page.waitFor(2000);
-    await page.screenshot({ path: `screenshots/protobuf.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/protobuf.png` });
 
     await (page as any).select("select", "cases.json");
     await page.waitFor(2000);
-    await page.screenshot({ path: `screenshots/json.png`, fullPage: true });
+    await page.screenshot({ path: `screenshots/json.png` });
 
     browser.close();
 })();
