d1a70b735592d6b0ddd11c32c2b841502d93728b
york yao <yaoao12306@outlook.com>
2018-05-16 05:53:53 +0800

    fix: CI failure on windows

--- clean-run.config.js
+++ clean-run.config.js
@@ -9,6 +9,6 @@ module.exports = {
   ],
   postScript: [
     'cd "[dir]" && yarn --production',
-    '[dir]/bin/types-as-schema demo/cases.ts --json demo/ --debug demo/debug.json --protobuf demo/cases.proto --graphql demo/cases.gql --reason demo/cases.re --ocaml demo/cases.ml --rust demo/cases.rs'
+    'node [dir]/dist/index.js demo/cases.ts --json demo/ --debug demo/debug.json --protobuf demo/cases.proto --graphql demo/cases.gql --reason demo/cases.re --ocaml demo/cases.ml --rust demo/cases.rs'
   ]
 }
