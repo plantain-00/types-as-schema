415982dcf48613680305e1c0773a28787fe34163
york yao <yaoao12306@outlook.com>
2018-11-10 23:50:59 +0800

    fix: graphql scalar JSON declaration

--- src/graphql-schema-generator.ts
+++ src/graphql-schema-generator.ts
@@ -2,6 +2,7 @@ import { TypeDeclaration, Type, ReferenceType, ObjectDeclaration, Member, Member
 
 const stageName = 'grapql schema generator'
 
+// tslint:disable-next-line:cognitive-complexity
 export function generateGraphqlSchema(declarations: TypeDeclaration[]) {
   const inputTypeSet = new Set<TypeDeclaration>()
   for (const typeDeclaration of declarations) {
@@ -9,11 +10,15 @@ export function generateGraphqlSchema(declarations: TypeDeclaration[]) {
   }
 
   const messages: string[] = []
+  let hasJSON = false
   for (const typeDeclaration of declarations) {
     if (typeDeclaration.kind === 'object') {
       const message = generateGraphqlSchemaOfObject(declarations, typeDeclaration, inputTypeSet)
       if (message) {
-        messages.push(message)
+        messages.push(message.result)
+        if (!hasJSON) {
+          hasJSON = message.hasJSON
+        }
       }
     } else if (typeDeclaration.kind === 'enum') {
       const members = typeDeclaration.members.map(m => `  ${m.name}`)
@@ -34,7 +39,8 @@ ${members.join('\n')}
 }`)
     }
   }
-  return messages.join('\n\n') + '\n'
+  const scalars = hasJSON ? `scalar JSON\n\n` : ''
+  return scalars + messages.join('\n\n') + '\n'
 }
 
 // tslint:disable-next-line:cognitive-complexity
@@ -79,46 +85,68 @@ function generateGraphqlSchemaOfObject(typeDeclarations: TypeDeclaration[], obje
     return undefined
   }
   const members = objectDeclaration.members.map(m => generateGraphqlSchemaOfObjectMember(typeDeclarations, m))
+  const hasJSON = members.some((m) => m.hasJSON)
   const type = inputTypeSet.has(objectDeclaration) ? 'input' : 'type'
-  return `${type} ${objectDeclaration.name} {
-${members.filter(m => m).join('\n')}
+  const result = `${type} ${objectDeclaration.name} {
+${members.filter(m => m.result).map(m => m.result).join('\n')}
 }`
+  return {
+    result,
+    hasJSON
+  }
 }
 
 function generateGraphqlSchemaOfObjectMember(typeDeclarations: TypeDeclaration[], member: Member) {
   const propertyType = getGraphqlSchemaProperty(typeDeclarations, member.type)
-  if (propertyType) {
+  let hasJSON = false
+  let result: string | undefined
+  if (propertyType.propertyType) {
+    hasJSON = propertyType.hasJSON
     let parameterList = ''
     if (member.parameters) {
-      parameterList = generateGraphqlSchemaOfParameters(typeDeclarations, member.parameters)
+      const parameterResult = generateGraphqlSchemaOfParameters(typeDeclarations, member.parameters)
+      parameterList = parameterResult.result
+      if (!hasJSON) {
+        hasJSON = parameterResult.hasJSON
+      }
     }
-    return `  ${member.name}${parameterList}: ${member.optional ? propertyType : propertyType + '!'}`
+    result = `  ${member.name}${parameterList}: ${member.optional ? propertyType.propertyType : propertyType.propertyType + '!'}`
   }
-  return undefined
+  return {
+    result,
+    hasJSON
+  }
 }
 
 function generateGraphqlSchemaOfParameters(typeDeclarations: TypeDeclaration[], memberParameters: MemberParameter[]) {
   const parameters: string[] = []
+  let hasJSON = false
   for (const parameter of memberParameters) {
     const parameterPropertyType = getGraphqlSchemaProperty(typeDeclarations, parameter.type)
-    if (parameterPropertyType) {
+    if (parameterPropertyType.propertyType) {
       if (parameter.optional) {
-        parameters.push(`${parameter.name}: ${parameterPropertyType}`)
+        parameters.push(`${parameter.name}: ${parameterPropertyType.propertyType}`)
       } else {
-        parameters.push(`${parameter.name}: ${parameterPropertyType}!`)
+        parameters.push(`${parameter.name}: ${parameterPropertyType.propertyType}!`)
       }
+      hasJSON = parameterPropertyType.hasJSON
     }
   }
-  return parameters.length > 0 ? `(${parameters.join(', ')})` : ''
+  return {
+    result: parameters.length > 0 ? `(${parameters.join(', ')})` : '',
+    hasJSON
+  }
 }
 
 // tslint:disable-next-line:cognitive-complexity
-function getGraphqlSchemaProperty(typeDeclarations: TypeDeclaration[], memberType: Type): string {
+function getGraphqlSchemaProperty(typeDeclarations: TypeDeclaration[], memberType: Type): { propertyType: string, hasJSON: boolean } {
   let propertyType = ''
+  let hasJSON = false
   if (memberType.kind === 'array') {
     const elementPropertyType = getGraphqlSchemaProperty(typeDeclarations, memberType.type)
-    if (elementPropertyType) {
-      propertyType = `[${elementPropertyType}]`
+    if (elementPropertyType.propertyType) {
+      propertyType = `[${elementPropertyType.propertyType}]`
+      hasJSON = elementPropertyType.hasJSON
     }
   } else if (memberType.kind === 'enum') {
     if (memberType.name === 'string') {
@@ -127,7 +155,9 @@ function getGraphqlSchemaProperty(typeDeclarations: TypeDeclaration[], memberTyp
       propertyType = memberType.name
     }
   } else if (memberType.kind === 'reference') {
-    propertyType = getGraphqlSchemaPropertyOfReference(typeDeclarations, memberType)
+    const referencePropertyType = getGraphqlSchemaPropertyOfReference(typeDeclarations, memberType)
+    propertyType = referencePropertyType.propertyType
+    hasJSON = referencePropertyType.hasJSON
   } else if (memberType.kind === 'number') {
     if (memberType.type === 'number'
       || memberType.type === 'float'
@@ -143,30 +173,44 @@ function getGraphqlSchemaProperty(typeDeclarations: TypeDeclaration[], memberTyp
   } else if (memberType.kind === 'map') {
     warn(memberType.position, stageName)
     propertyType = 'JSON'
+    hasJSON = true
   } else if (memberType.kind === 'union') {
     // tslint:disable-next-line:no-collapsible-if
     if (memberType.members.some(m => m.kind !== 'reference')) {
       warn(memberType.position, stageName)
       propertyType = 'JSON'
+      hasJSON = true
     }
+  } else if (memberType.kind === undefined) {
+    propertyType = 'JSON'
+    hasJSON = true
   }
-  return propertyType
+  return {
+    propertyType,
+    hasJSON
+  }
 }
 
 function getGraphqlSchemaPropertyOfReference(typeDeclarations: TypeDeclaration[], memberType: ReferenceType) {
   const typeDeclaration = typeDeclarations.find(m => m.name === memberType.name)
+  let hasJSON = false
+  let propertyType = memberType.name
   if (typeDeclaration) {
     if (typeDeclaration.kind === 'enum' && typeDeclaration.type === 'string') {
-      return 'String'
-    }
-    if (typeDeclaration.kind === 'object' && typeDeclaration.members.length === 0) {
+      propertyType = 'String'
+    } else if (typeDeclaration.kind === 'object' && typeDeclaration.members.length === 0) {
       warn(memberType.position, stageName)
-      return 'JSON'
-    }
-    if (typeDeclaration.kind === 'union' && typeDeclaration.members.some(m => m.kind !== 'reference')) {
+      propertyType = 'JSON'
+      hasJSON = true
+      // tslint:disable-next-line:no-duplicated-branches
+    } else if (typeDeclaration.kind === 'union' && typeDeclaration.members.some(m => m.kind !== 'reference')) {
       warn(memberType.position, stageName)
-      return 'JSON'
+      propertyType = 'JSON'
+      hasJSON = true
     }
   }
-  return memberType.name
+  return {
+    propertyType,
+    hasJSON
+  }
 }
