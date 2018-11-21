8c6b15e1c47ef259f98d173695c59a14ddc44e93
york yao <yaoao12306@outlook.com>
2018-11-22 07:23:06 +0800

    fix: resolve return type

--- src/graphql-root-type-generator.ts
+++ src/graphql-root-type-generator.ts
@@ -80,7 +80,9 @@ export type DeepReturnType<T> = {
   [P in keyof T]: T[P] extends Array<infer U>
     ? Array<DeepReturnType<U>>
     : T[P] extends (...args: any[]) => infer R
-      ? R
+      ? R extends Promise<infer U>
+        ? U
+        : R
       : DeepReturnType<T[P]>
 }
 
