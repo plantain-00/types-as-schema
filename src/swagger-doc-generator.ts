7e67203228d438bf78babe9089c8423fa409fe06
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-25 23:53:09 +0000

    Deploy plantain-00/types-as-schema to github.com/plantain-00/types-as-schema.git:gh-pages

--- src/swagger-doc-generator.ts
+++ src/swagger-doc-generator.ts
@@ -1,29 +1,33 @@
-import { TypeDeclaration } from './utils'
+import { TypeDeclaration, Type } from './utils'
 
-export function generateSwaggerDoc(_typeDeclarations: TypeDeclaration[]) {
-  const result = {
-    swagger: '2.0',
-    paths: {
-      '/pet/{id}': {
-        get: {
-          operationId: 'getPetById',
-          parameters: [
-            {
-              name: 'petId',
-              required: true,
-              type: 'integer'
-            }
-          ],
-          responses: {
-            200: {
-              schema: {
-                $ref: '#/definitions/Pet'
-              }
-            }
+export function generateSwaggerDoc(typeDeclarations: TypeDeclaration[]) {
+  const paths: { [path: string]: { [method: string]: any } } = {}
+  for (const typeDeclaration of typeDeclarations) {
+    if (typeDeclaration.kind === 'function'
+      && typeDeclaration.path
+      && typeDeclaration.method) {
+      if (!paths[typeDeclaration.path]) {
+        paths[typeDeclaration.path] = {}
+      }
+      paths[typeDeclaration.path][typeDeclaration.method] = {
+        operationId: typeDeclaration.name,
+        parameters: typeDeclaration.parameters.map((parameter) => ({
+          name: parameter.name,
+          required: !parameter.optional,
+          in: 'query',
+          ...getType(parameter.type)
+        })),
+        responses: {
+          200: {
+            ...getType(typeDeclaration.type)
           }
         }
       }
-    },
+    }
+  }
+  const result = {
+    swagger: '2.0',
+    paths,
     definitions: {
       Pet: {
         type: 'object',
@@ -59,4 +63,17 @@ export function generateSwaggerDoc(_typeDeclarations: TypeDeclaration[]) {
     }
   }
   return JSON.stringify(result, null, 2)
+}
+
+function getType(type: Type) {
+  if (type.kind === 'reference') {
+    return {
+      schema: {
+        $ref: `#/definitions/${type.name}`
+      }
+    }
+  }
+  return {
+    type: type.kind
+  }
 }
